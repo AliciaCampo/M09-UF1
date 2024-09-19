00a3866f038932468d09c2cb7043d25dc26c8d5e
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2024-09-19 19:11:30 +0200

    estructura del programa anotada

--- Activitat_1/Rot13.java
+++ Activitat_1/Rot13.java
@@ -3,17 +3,25 @@ package Activitat_1;
 //programa que encripta y desencripta mensajes con cifrado cesar 13
 public class Rot13 {
     public static String codificado = "";
+    public static final char [] diccionarioMin ={'a','b','c','d'};
+    public static final char [] diccionarioMax = {'A','B','C','D'};
 
     public static void main(String[] args) {
-        System.out.println("Introduce una palabra para cifrar :");
-        /*String entrada = Entrada.readLine();*/
-        //String resultado = xifraRot13(entrada);
-       // System.out.println("El mensaje :" + entrada + "se ha cifrado  a " + resultado);
+        System.out.println("Texto a cifrar :  Hola Alicia");
+        String entrada = "Hola Alicia";
+        String resultado = xifraRot13(entrada);
+        System.out.println("El mensaje :" + entrada + "se ha cifrado  a " + resultado);
+        
         
         
     }
 
     public static  String xifraRot13 (String mensaje){
+        char [] textoChar = new char[mensaje.length()] ;
+        for (int i = 0 ; i < mensaje.length() ; i ++){
+            textoChar[i] = mensaje.charAt(i);
+        }
+        //comparar el textoChar con los char de clase de diccionario
 
         return codificado ;
 
