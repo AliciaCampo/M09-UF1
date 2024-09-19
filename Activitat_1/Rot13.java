cdae1ed6ec84ea49dfb8c3ece71bd69324c2a022
AliciaCampo <2023_alicia.campo@iticbcn.cat>
2024-09-19 17:30:21 +0200

    primera versio

---
+++ Activitat_1/Rot13.java
@@ -0,0 +1,27 @@
+package Activitat_1;
+
+//programa que encripta y desencripta mensajes con cifrado cesar 13
+public class Rot13 {
+    public static String codificado = "";
+
+    public static void main(String[] args) {
+        System.out.println("Introduce una palabra para cifrar :");
+        String entrada = Entrada.readLine();
+        String resultado = xifraRot13(entrada);
+        System.out.println("El mensaje :" + entrada + "se ha cifrado  a " + resultado);
+        
+        
+    }
+
+    public static  String xifraRot13 (String mensaje){
+
+        return codificado ;
+
+    }
+    public static  String desxifraRot13 (String mensaje){
+
+        return codificado ;
+
+    }
+
+}
