0ab658cb627fab8a4a7e52ebbbbeae4fcbf610dc
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2024-09-23 20:28:37 +0200

    codigo arreglado en desxifraRot13 , y añadido modulo auxiliar para hacer la rotación

--- Activitat_1/Rot13.java
+++ Activitat_1/Rot13.java
@@ -1,81 +1,68 @@
 package Activitat_1;
-//programa que encripta y desencripta mensajes con cifrado cesar 13
 public class Rot13 {
-    public static final char [] diccionarioMin ={
+    public static final char[] diccionarioMin = {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-        'y', 'z', 'á', 'é', 'í', 'ó', 'ú', 'ñ', 'ç'};
-    public static final char [] diccionarioMax = {
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z', 'á', 'é', 'í', 'ó', 'ú', 'ñ', 'ç'
+    };
+    public static final char[] diccionarioMax = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
         'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
         'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-        'Y', 'Z', 'Á', 'É', 'Í', 'Ó', 'Ú', 'Ñ', 'Ç'};
-
+        'Y', 'Z', 'Á', 'É', 'Í', 'Ó', 'Ú', 'Ñ', 'Ç'
+    };
     public static void main(String[] args) {
-        System.out.println("Texto a cifrar :  Hola Alicia");
-        String entrada = "Hola Alicia";
-        //cifrar el texto
-        String resultado = xifraRot13(entrada);
-        System.out.println("El mensaje :" + entrada + "se ha cifrado  a " + resultado);
-        //descrifrar el texto 
-        String resultadoDescifrado = desxifraRot13(resultado);
-        System.out.println("El mensaje cifrado  : "+ resultado+ "se ha descifrado a  : "+ resultadoDescifrado);
+        String entrada = "El conocimiento es la llave del éxito.";
+        System.out.println("Texto a cifrar: " + entrada);
+        String resultadoCifrado = xifraRot13(entrada);
+        System.out.println("El mensaje '" + entrada + "' se ha cifrado a: " + resultadoCifrado);
+        String resultadoDescifrado = desxifraRot13(resultadoCifrado);
+        System.out.println("El mensaje cifrado '" + resultadoCifrado + "' se ha descifrado a: " + resultadoDescifrado);
     }
-    //funcion que cifra con ROT13
-    public static  String xifraRot13 (String mensaje){
+    public static String xifraRot13(String mensaje) {
         String codificado = "";
-        for (int i = 0 ; i < mensaje.length() ; i ++){
+        for (int i = 0; i < mensaje.length(); i++) {
             char letra = mensaje.charAt(i);
-            if (Character.isLowerCase(letra)){
-                //busca la letra en el diccionario minuscula y ejecuta el desplazamiento
-                for(int j = 0 ; j < diccionarioMin.length; j++){
-                    if (diccionarioMin[j] == letra){
-                        codificado += diccionarioMin[(j+13)% diccionarioMin.length];
-                        break;
-                    }
-                }
-            }else if (Character.isUpperCase(letra)){
-                //busca en la letra en el diccionario mayuscula y ejecuta el desplazamiento
-                for (int l = 0; l < diccionarioMax.length ; l ++){
-                    if(diccionarioMax[l] == letra){
-                        codificado += diccionarioMax[(l+13)%diccionarioMax.length];
-                        break;
-                    }
-                }
-            }
-            else{
-                codificado += letra ;//espacios y numeros
+            if (Character.isLowerCase(letra)) {
+                codificado += desplazarRot13(letra, diccionarioMin, 13);
+            } 
+            else if (Character.isUpperCase(letra)) {
+                codificado += desplazarRot13(letra, diccionarioMax, 13);
+            } 
+            else {
+                codificado += letra;
             }
         }
-        return codificado ;
-    }
-    public static  String desxifraRot13 (String mensaje){
-        String descodificado = "";
-        for (int i = 0 ; i < mensaje.length() ; i ++){
-            char letra = mensaje.charAt(i);
-            if (Character.isLowerCase(letra)){
-                //busca la letra en el diccionario minuscula y ejecuta el desplazamiento
-                for(int j = 0 ; j < diccionarioMin.length; j++){
-                    if (diccionarioMin[j] == letra){
-                        descodificado += diccionarioMin[(j-13)% diccionarioMin.length];
-                        break;
-                    }
-                }
-            }else if (Character.isUpperCase(letra)){
-                //busca en la letra en el diccionario mayuscula y ejecuta el desplazamiento
-                for (int l = 0; l < diccionarioMax.length ; l ++){
-                    if(diccionarioMax[l] == letra){
-                        descodificado += diccionarioMax[(l-13)%diccionarioMax.length];
-                        break;
-                    }
-                }
-            }
-            else{
-                descodificado += letra ;//espacios y numeros
-            }
-        }
-        return descodificado ;
         
+        return codificado;
+    }
+    public static String desxifraRot13(String mensaje) {
+        String descodificado = "";
+        for (int i = 0; i < mensaje.length(); i++) {
+            char letra = mensaje.charAt(i);
+            if (Character.isLowerCase(letra)) {
+                descodificado += desplazarRot13(letra, diccionarioMin, -13);
+            } 
+            else if (Character.isUpperCase(letra)) {
+                descodificado += desplazarRot13(letra, diccionarioMax, -13);
+            } 
+            else {
+                descodificado += letra;
+            }
+        }
+        return descodificado;
+    }
+    public static char desplazarRot13(char letra, char[] diccionario, int desplazamiento) {
+        for (int i = 0; i < diccionario.length; i++) {
+            if (diccionario[i] == letra) {
+                int nuevaPos = (i + desplazamiento) % diccionario.length;
+                if (nuevaPos < 0) {
+                    nuevaPos += diccionario.length;
+                }
+                return diccionario[nuevaPos];
+            }
+        }
+        return letra;
     }
 }
