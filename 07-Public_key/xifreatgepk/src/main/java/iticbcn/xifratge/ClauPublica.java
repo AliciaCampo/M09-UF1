c364384cb92407bec225f7c1b620014f081c921d
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2024-11-07 17:27:59 +0100

    primera versio falta compilar el Main i fer proves

---
+++ 07-Public_key/xifreatgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -0,0 +1,30 @@
+package iticbcn.xifratge;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import javax.crypto.Cipher;
+
+public class ClauPublica {
+
+    // Método para generar un par de claves RSA
+    public KeyPair generaParellClausRSA() throws Exception {
+        KeyPairGenerator claves = KeyPairGenerator.getInstance("RSA");
+        claves.initialize(2048); // Tamaño seguro de clave en bits
+        return claves.generateKeyPair();
+    }
+    // Método para cifrar un mensaje usando la clave pública
+    public byte[] xifraRSA(String msg, PublicKey clauPublica) throws Exception {
+        Cipher cifrado = Cipher.getInstance("RSA");
+        cifrado.init(Cipher.ENCRYPT_MODE, clauPublica);
+        return cifrado.doFinal(msg.getBytes());
+    }
+    // Método para descifrar un mensaje cifrado usando la clave privada
+    public String desxifraRSA(byte[] msgXifrat, PrivateKey clauPrivada) throws Exception {
+        Cipher descifrado = Cipher.getInstance("RSA");
+        descifrado.init(Cipher.DECRYPT_MODE, clauPrivada);
+        byte[] mensajeDescifrado = descifrado.doFinal(msgXifrat);
+        return new String(mensajeDescifrado);
+    }
+}
